8574e9b3064b81450794a72ff793b554eaa8b599
Wankupi <2893353848@qq.com>
2023-11-30 00:30:22 +0800

    perf: MemoryController

--- src/cache/MemoryController.v
+++ src/cache/MemoryController.v
@@ -5,7 +5,7 @@ module MemoryController (
 
     input  wire [ 7:0] mem_din,   // data input bus
     output wire [ 7:0] mem_dout,  // data output bus
-    output wire [31:0] mem_a,     // address bus (only 17:0 is used)
+    output reg  [31:0] mem_a,     // address bus (only 17:0 is used)
     output wire        mem_wr,    // write/read signal (1 for write)
 
     input  wire        inst_valid,
@@ -35,15 +35,16 @@ module MemoryController (
     reg [31:0] result;
     reg [1:0] ready;
 
-    wire need_work = inst_valid || data_valid;
+    wire need_inst = inst_valid && !(addr == inst_addr && ready[Inst]);
+    wire need_work = need_inst || data_valid;
     wire inst_or_data = data_valid;
 
     assign mem_wr = rw;
-    assign mem_a = addr;
     assign data_res = result;
     assign inst_res = {mem_din, result[23:0]};
     assign mem_dout = toMem;
-    assign inst_ready = ready[Inst], data_ready = ready[Data];
+    assign inst_ready = ready[Inst] && !need_inst;
+    assign data_ready = ready[Data];
 
     always @(posedge clk_in) begin
         if (rst_in) begin
@@ -55,7 +56,7 @@ module MemoryController (
             toMem <= 0;
             result <= 0;
             ready <= 0;
-            // TODO
+            mem_a <= 0;
         end
         else if (!rdy_in) begin
             // do nothing
@@ -67,6 +68,7 @@ module MemoryController (
                     work_cycle <= 0;
                     work_type <= inst_or_data == Inst ? Inst : Data;
                     rw = inst_or_data == Inst ? 0 : data_wr;
+                    mem_a  <= inst_or_data == Inst ? inst_addr : data_addr;
                     addr   <= inst_or_data == Inst ? inst_addr : data_addr;
                     result <= data_data;
                     toMem  <= data_data[7:0];
@@ -75,18 +77,18 @@ module MemoryController (
             end
             else begin  // working
                 work_cycle <= work_cycle + 1;
-                if (work_cycle < 3) begin
-                    addr <= addr + 1;
-                end
                 case (work_cycle)
                     0: begin
+                        mem_a <= {addr[31:2], 2'b01};
                         toMem <= result[15:8];
                     end
                     1: begin
+                        mem_a <= {addr[31:2], 2'b10};
                         toMem <= result[23:16];
                         result[7:0] <= mem_din;
                     end
                     2: begin
+                        mem_a <= {addr[31:2], 2'b11};
                         toMem <= result[31:24];
                         result[15:8] <= mem_din;
                     end
@@ -95,11 +97,9 @@ module MemoryController (
                         ready[work_type] <= 1;
                         working <= 0;
                     end
-                    // 4: begin
-                    //     result[31:24] <= mem_din;
-                    // end
                 endcase
             end
         end
     end
+
 endmodule
